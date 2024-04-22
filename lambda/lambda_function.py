65c28b3187e35d31683056dcee3dac1b4aa70bf9
smalo0 <128261499+smalo0@users.noreply.github.com>
2024-04-21 20:13:22 -0400

    Update lambda_function.py

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,23 +1,30 @@
 import json
-import boto3
 from cassandra.cluster import Cluster
 
-#s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
+    # print statements available for troubleshooting
+    if 'sensor_id' in event:
+        sensor_id = event['sensor_id']
+        print(sensor_id)
+
+    function_name = context.function_name
+    print(function_name)
+
     # Connect to Cassandra
     cluster = Cluster(['34.95.4.234'], port=9042)
     session = cluster.connect("motion")
-    
+
     # Prepare and execute the query
-    preparedStatement = session.prepare("SELECT * FROM motion_sensor_nosec WHERE state='True' ALLOW FILTERING;")
-    rows = session.execute(preparedStatement)
-    
+    prepared_statement = session.prepare("SELECT * FROM motion_sensor_nosec WHERE state='True' ALLOW FILTERING;")
+    rows = session.execute(prepared_statement)
+
     # Process the results
     result = []
     for row in rows:
         result.append(dict(row))
-    
+        print(row)
+
     # Close the Cassandra session and cluster
     session.shutdown()
     cluster.shutdown()
@@ -26,23 +33,29 @@ def lambda_handler(event, context):
     file_path = "/data/motion_sensor_data.json"
     with open(file_path, "w") as file:
         file.write(json_data)
-    
-    # Retrieve AWS credentials and region from environment variables
-    # aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
-    # aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    # region = os.environ.get('AWS_DEFAULT_REGION')
-    # Store the results in S3
-    # bucket_name = 'iot-sensordata-bucket'
-    # object_key = 'motion_sensor_data.json'
-    # s3_client = boto3.client("s3", region_name=region, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-    # s3_client.upload_file(json_data, bucket_name, object_key)
-    # s3.put_object(
-        # Bucket=bucket_name,
-        # Key=object_key,
-        # Body=json_data
-    # )
-    
+
     return {
         'statusCode': 200,
         'body': json.dumps({'message': 'Data retrieved successfully'})
     }
+
+
+event1 = {
+    'time': '2024-04-21T12:00:00Z',
+    'motion': 'detected',
+    'sensor_id': '77475e31-3c2f-4a57-8a3a-2f06883deed8'
+}
+
+
+class DummyContext:
+    def __init__(self):
+        self.function_name = 'gcp_aws_data_connector'
+        self.function_version = '1.0'
+        # self.aws_request_id = '1234567890'
+
+
+context1 = DummyContext()
+
+lambda_result = lambda_handler(event1, context1)
+print(lambda_result)
+
